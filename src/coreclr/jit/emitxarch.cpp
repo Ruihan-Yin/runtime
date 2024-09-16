729e7e3fd4454d7c28d30ed027aded2d6905d67c
Ruihan-Yin <ruihan.yin@intel.com>
2024-09-16 15:23:29 -0700

    add emitter unit tests, and fix encoding error for CMOVcc

--- src/coreclr/jit/emitxarch.cpp
+++ src/coreclr/jit/emitxarch.cpp
@@ -16387,7 +16387,7 @@ BYTE* emitter::emitOutputRRR(BYTE* dst, instrDesc* id)
             case EA_2BYTE:
             case EA_4BYTE:
                 // Set the 'w' bit to get the large version
-                code |= 0x1;
+                code = insIsCMOV(ins)? code : (code | (0x01));
                 break;
 
 #ifdef TARGET_AMD64
@@ -16396,7 +16396,7 @@ BYTE* emitter::emitOutputRRR(BYTE* dst, instrDesc* id)
                 // Don't need to zero out the high bits explicitly
                 code = AddRexWPrefix(id, code); // TODO-APX : Revisit. does xor or other cases need to be handled differently? see emitOutputRR
                 // Set the 'w' bit to get the large version
-                code |= 0x1;
+                code = insIsCMOV(ins)? code : (code | (0x01));
                 break;
 
 #endif // TARGET_AMD64

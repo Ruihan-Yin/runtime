0bd46809097ccb21e01dbb4fa4b3c51f2170cb62
Ruihan-Yin <ruihan.yin@intel.com>
2024-10-23 17:11:59 -0700

    make sure the APX debug knob is only available under debug build.

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -9895,10 +9895,12 @@ public:
     //
     bool canUseApxEncodings() const
     {
+#ifdef DEBUG
         if(JitConfig.JitBypassAPXCheck())
         {
             return true;
         }
+#endif // DEBUG
         return compOpportunisticallyDependsOn(InstructionSet_APX);
     }
 
